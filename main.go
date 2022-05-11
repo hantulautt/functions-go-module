a8b9bd1829ba7c24cef37a8d98fc2757f921b51e
arakoswara <arakoswara2@gmial.com>
2022-05-11 11:36:29 +0700

    update layout str2time

--- main.go
+++ main.go
@@ -20,7 +20,7 @@ func GenerateString(total int) string {
 }
 
 func Str2Time(str string) int64 {
-	layout := "2006-01-02"
+	layout := "2006-01-02 15:04:05"
 	t, err := time.Parse(layout, str)
 	if err != nil {
 		return 0
